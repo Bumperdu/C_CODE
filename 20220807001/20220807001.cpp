538b02c7d9fdba0266ede14a4f1c6fa5b3067272
Bumperdu <18395476421@163.com>
2022-08-11 19:51:29 +0800

    修复旧bug  添加新bug

---
+++ 20220807001/20220807001.cpp
@@ -0,0 +1,12 @@
+#include  <stdio.h>
+
+int main() {
+	int num1 = 0;
+	int num2 = 0;
+	int sum = 0;
+	printf("请输入两个操作数：>");
+	scanf_s("%d %d", &a, &b);
+	sum = num1 + num2;
+	printf("sum=%d", sum);
+	return 0;
+}
